b475d3a4dd97c4a89695ee75d2e0306b62874d19
coma-dwc <125862959+tamagomapon@users.noreply.github.com>
2023-03-22 08:49:22 +0000

    [finish]応用課題1 RSpecテスト完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_061455) do
+ActiveRecord::Schema.define(version: 2023_03_22_051406) do
 
   create_table "books", force: :cascade do |t|
     t.text "body"
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
